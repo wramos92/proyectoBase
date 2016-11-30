39b3510407d48c3b13d2ba997f5b5b59eb27fbf4
wramos92 <wramosc92@gmail.com>
2016-11-29 20:35:12 -0400

    objetos

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
-
+var reclamo = { tipo:"odeco",observacion:"malservicio" ,reclamo:"29/11/2016"};
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'PROYECTO FINAL',
+  	                    subtitle: 'avances en computacion',
+  	                    reclamo:reclamo });
 });
 
 module.exports = router;
